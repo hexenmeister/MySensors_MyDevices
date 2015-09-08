1ccccd8d25b091c36f40e32462ab162f904b3eff
hexenmeister <mail@s6z.de>
2015-09-08 22:00:51 +0200

    Initial

---
+++ WriteDesiredNodeID/WriteDesiredNodeID.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <EEPROM.h>  
+#include <MySensor.h>  
+
+#define NODE_ID 250
+
+void setup()  
+{ 
+  Serial.begin(BAUD_RATE);
+  
+  Serial.print("Write desiren NodeID: ");
+  Serial.print(NODE_ID);
+  Serial.println(" Please wait...");
+
+  hw_writeConfig(EEPROM_NODE_ID_ADDRESS, NODE_ID);
+
+  Serial.println("Done. You're ready to go!");
+}
+
+void loop()      
+{ 
+  // Nothing to do here...
+}
+
